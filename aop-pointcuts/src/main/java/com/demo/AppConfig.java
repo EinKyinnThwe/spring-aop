9f02dd1c9ffb2236eea4dd7fee632d87aedf5f39
EinKyinnThwe <einkyinnthwe@gmail.com>
2023-03-04 11:10:24 +0630

    aop pointcut

--- aop-pointcuts/src/main/java/com/demo/AppConfig.java
+++ aop-pointcuts/src/main/java/com/demo/AppConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @ComponentScan
 @Configuration
+@EnableAspectJAutoProxy
 public class AppConfig {
 }
