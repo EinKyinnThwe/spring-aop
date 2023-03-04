48b614bd1efc4971bdec93ebaa937553a384adf7
EKThwe <einkyinnthwe@gmail.com>
2023-03-04 09:27:32 +0630

    benifit aop

---
+++ aop-pointcuts/src/main/java/com/demo/aspect/MyCurrencyServiceAspect.java
@@ -0,0 +1,83 @@
+package com.demo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class MyCurrencyServiceAspect {
+    @Pointcut("within(com.demo.service.*)")
+    public void withinPointCut() {}
+    //@Before("withinPointCut()")
+    public void beforeCurrencyAdvice(JoinPoint joinPoint) {
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    //@Pointcut("@within(com.demo.annotation.Secured)")
+    public void annotationWithinPointCut() {}
+    public void beforeWithinAdvice(JoinPoint joinPoint) {
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    @Pointcut("@annotation(com.demo.annotation.InTransaction)")
+    public void annotation(){}
+    //@Before("annotationWithinPointCut()")
+    public void annotationBeforeAdvice(JoinPoint joinPoint) {
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    @Pointcut("bean(my_bean)")
+    public void beanPointCut(){}
+    //@Before("beanPointCut()")
+    public void myBeanBeforeAnnotation(JoinPoint joinPoint) {
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    @Pointcut("args(int , double )")
+    public void argsPointCut(){}
+    //@Before("argsPointCut()")
+    public void argsBeforeAdvice(JoinPoint joinPoint){
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    @Pointcut("@args(com.demo.annotation.Validated)")
+    public void annotationArgsPointCut(){}
+    //@Before("annotationArgsPointCut()")
+    public void annotationBeforeCurrencyTypeAdvice(JoinPoint joinPoint) {
+        System.out.println(
+                String.format("%s is invoked before with:%s",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()))
+        );
+    }
+    //@AfterReturning(value = "annotationArgsPointCut()", returning = "name")
+    public void afterReturningAdvice(JoinPoint joinPoint, String name) {
+        System.out.println(
+                String.format("%s is invoked before with:%s and return value:[%s]",
+                        joinPoint.getSignature().getName(),
+                        Arrays.toString(joinPoint.getArgs()), name)
+        );
+    }
+    @AfterThrowing(value = "annotationArgsPointCut()", throwing = "exception")
+    public void afterThrowingAdvice(JoinPoint joinPoint, Exception exception) {
+        System.out.println("Exception caught::" + exception);
+    }
+}
